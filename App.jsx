6a0e96aba92864efe44727a1948016b962d3aec4
dkarthikprojects <dkarthik952@gmail.com>
2026-03-26 13:31:09 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-  <rect width="32" height="32" rx="6" fill="#0F172A"/>
-  <text x="6" y="23" font-size="20" font-family="system-ui" font-weight="800" fill="#38BDF8">M</text>
-</svg>
